60a628cf6fcb0c9862edff8ce5b98f2b42cd3cc1
Bloemerang <enyalie.is@gmail.com>
2011-04-15 23:58:05 -0500

    Changed name and desc to vars to make definition a little less verbose.

--- src/orb/game/LittleHouse.scala
+++ src/orb/game/LittleHouse.scala
@@ -5,27 +5,27 @@ import orb.library.runtime.Adventure
 import orb.library._
 
 object FrontLawn extends Location {
-    val name = "Lawn"
-    val desc = "A well-kept lawn. To the north is a little house. " +
-               "a screen door leads on to the porch"
+    name = "Lawn"
+    desc = "A well-kept lawn. To the north is a little house. " +
+           "a screen door leads on to the porch"
 
     n = new Door(FrontPorch, FrontLawn, true)
 }
 
 object FrontPorch extends Location {
-    val name = "Front Porch"
-    val desc = "A nice, tidy porch with a screen door. " +
-               "The front door is to the north"
+    name = "Front Porch"
+    desc = "A nice, tidy porch with a screen door. " +
+           "The front door is to the north"
 
     s = new Door(FrontLawn, FrontPorch, true)
     n = new Door(Foyer, FrontPorch, true)
 }
 
 object Foyer extends Location {
-    val name = "Foyer"
-    val desc = "A large, spacious foyer, painted red. " +
-               "To the east is a door to the cellar; " +
-               "to the west is a door into the bathroom."
+    name = "Foyer"
+    desc = "A large, spacious foyer, painted red. " +
+           "To the east is a door to the cellar; " +
+           "to the west is a door into the bathroom."
 
     s = new Door(FrontPorch, Foyer, true)
     e = new Door(CellarStairs, Foyer, true)
@@ -33,15 +33,15 @@ object Foyer extends Location {
 }
 
 object CellarStairs extends Location {
-    val name = "Cellar Stairs"
-    val desc = "The cellar stairs"
+    name = "Cellar Stairs"
+    desc = "The cellar stairs"
 
     w = new Door(Foyer, CellarStairs, true)
 }
 
 object Bathroom extends Location {
-    val name = "Bathroom"
-    val desc = "The bathroom"
+    name = "Bathroom"
+    desc = "The bathroom"
 
     e = new Door(Foyer, Bathroom, true)
 }
